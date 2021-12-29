f6a01d0151efd93617f3da34557b8fb553a389f0
mdn33 <54626709+mdn33@users.noreply.github.com>
2021-12-29 18:56:54 +0100

    Update main.py

--- Day18/main.py
+++ Day18/main.py
@@ -1,43 +1,129 @@
 import json
+import numpy as np
 from math import *
+from itertools import permutations
 
-
-def split(nested,found):
+class snailfish_number():
     
-    if isinstance(nested, int):
-            if nested > 10 and found[0] == 0:
-                split_left = floor(nested/2)
-                split_right = ceil(nested/2)
-                found[0] = 1
-                return [split_left,split_right]
-            else:
-                return nested
-            
-    elif (isinstance(nested, list) and any(isinstance(i, list) for i in nested) == False):
-        return [nested[0],nested[1]]
-    
-    else:
-        split_left = split(nested[0],found)
-        split_right = split(nested[1],found)
-            
-        return [split_left,split_right]
-
-def explode(nested,found,level):
-    
-    if isinstance(nested, int):
-        return
-    elif level == 4:
-        print(nested)
-        return
+    def __init__(self, string):
+        self.levels = []
+        self.numbers = []
         
-    else:
-        split_left =  explode(nested[0],found,level+1)
-        split_right = explode(nested[1],found,level+1)
-            
-        #return [split_left,split_right]
+        inp_list = json.loads(string)
+        self.initialize(inp_list,self.levels,-1,self.numbers)
         
-input_string = '[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]'
-input_string = json.loads(input_string)
-n = parse_input(input_string)
-splitted = [0]
-reduced_number = split(input_string,splitted)
+        
+    def initialize(self,inp_list,res,level,numbers):
+    
+        if isinstance(inp_list, int):
+            res.append(level)
+            numbers.append(inp_list)
+            return
+
+        self.initialize(inp_list[0],res,level+1,numbers)
+        self.initialize(inp_list[1],res,level+1,numbers)
+     
+    def explode(self):
+    
+        couple = -1
+        for i in range(len(self.levels)):
+            if self.levels[i] == 4:
+                couple = i
+                break
+
+        if couple != -1:
+            if couple-1 >= 0:
+                self.numbers[couple-1] += self.numbers[couple]
+            if couple+2 < len(self.numbers):
+                self.numbers[couple+2] += self.numbers[couple+1]
+
+            self.numbers[couple] = 0
+            self.levels[couple] -= 1
+            self.numbers.pop(couple+1)
+            self.levels.pop(couple+1)
+            return True
+        else:
+            return False
+        
+    def split(self):
+
+        couple = -1
+        for i in range(len(self.numbers)):
+            if self.numbers[i] >= 10:
+                couple = i
+                break
+
+        if couple != -1:
+            split_left = floor(self.numbers[couple]/2)
+            split_right = ceil(self.numbers[couple]/2)
+
+            self.numbers[couple] = split_left
+            self.numbers.insert(couple+1,split_right)
+
+            self.levels[couple] += 1
+            self.levels.insert(couple+1,self.levels[couple])
+            return True
+        else:
+            return False
+
+    def reduce(self):
+        while(1):
+            if self.explode() == False:
+                if self.split() == False:
+                    break
+                    
+    def get_magnitude(self):
+        
+        while len(self.levels) > 1:
+            for i in range(len(self.levels)):
+                if i+1 < len(self.levels):
+                    if self.levels[i] == self.levels[i+1]:
+                        magnitude = 3*self.numbers[i] + 2*self.numbers[i+1]
+                        self.numbers[i] = magnitude
+                        self.levels[i] -= 1
+                        self.levels.pop(i+1)
+                        self.numbers.pop(i+1)
+                        break
+                        
+        return self.numbers[0]
+                        
+    def add(self,snailfish_numb):
+        self.levels.extend(snailfish_numb.levels)
+        self.numbers.extend(snailfish_numb.numbers)
+        self.levels = list(map(lambda x:x+1, self.levels))
+        self.reduce()
+
+        
+#Part 1    
+with open('input.txt') as  fd:
+    
+    inp_list = fd.readline().strip()
+    s = snailfish_number(inp_list)
+    
+    lines = fd.read().splitlines()
+    for line in lines:
+        s_new = snailfish_number(line)
+        s.add(s_new)
+            
+print('Answer Part A:', s.get_magnitude())
+
+
+#Part 2
+snail = []
+max_magn = -1
+
+with open('input.txt') as  fd:
+    lines = fd.read().splitlines()
+    for line in lines:
+        snail.append(line)
+        
+for p in list(permutations([i for i in range(len(snail))],2)):
+    
+    s1 = snailfish_number(snail[p[0]])
+    s2 = snailfish_number(snail[p[1]])
+    s1.add(s2)
+    m = s1.get_magnitude()
+    if m > max_magn:
+        max_magn = m
+          
+print('Answer Part B', max_magn)
